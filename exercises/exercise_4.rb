5e3005fc16ff48f98ff31725a3b4417779931a7b
Kyle Tycholiz <tycholiz22@hotmail.com>
2018-09-11 16:36:00 -0700

    assignment4

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,27 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+surrey = Store.create
+surrey.annual_revenue = 224000
+surrey.mens_apparel = false
+surrey.womens_apparel = true
+surrey.save
+
+whistler = Store.create
+whistler.annual_revenue = 1900000
+whistler.mens_apparel = true
+whistler.womens_apparel = false
+whistler.save
+
+yaletown = Store.create
+yaletown.annual_revenue = 430000
+yaletown.mens_apparel = true
+yaletown.womens_apparel = true
+yaletown.save
+
+puts "-----------------------------------------------------------"
+
+@mens_stores = Store.where(mens_apparel: true)
+@mens_stores.each do |store|
+  puts store.name, store.annual_revenue
+end
