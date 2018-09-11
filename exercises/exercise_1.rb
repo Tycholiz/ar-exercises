7124317602ed4f8dcfe7f69bd0e9b49f4f7e2859
Kyle Tycholiz <tycholiz22@hotmail.com>
2018-09-11 23:17:35 +0000

    exercise 2

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -8,15 +8,18 @@ burnaby = Store.create
 burnaby.annual_revenue = 300000
 burnaby.mens_apparel = true
 burnaby.womens_apparel = true
+burnaby.save
 
 richmond = Store.create
 richmond.annual_revenue = 1260000
 richmond.mens_apparel = false
 richmond.womens_apparel = true
+richmond.save
 
 gastown = Store.create
 gastown.annual_revenue = 190000
 gastown.mens_apparel = true
 gastown.womens_apparel = false
+gastown.save
 
 puts Store.count
--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,9 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.first
+@store2 = Store.second
+p @store1
+p @store2
+@store1.name = "Best Location"
+@store1.save
