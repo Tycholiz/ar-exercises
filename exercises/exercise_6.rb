a5142a3c1c630f94af74628c6e8f7305cabdda09
Kyle Tycholiz <tycholiz22@hotmail.com>
2018-09-11 17:40:14 -0700

    finish assignment

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -4,8 +4,26 @@ require_relative './exercise_2'
 require_relative './exercise_3'
 require_relative './exercise_4'
 require_relative './exercise_5'
+require 'active_record'
 
 puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+class Store < ActiveRecord::Base
+  has_many :employees
+end
+
+class Employee < ActiveRecord::Base
+  belongs_to :store
+end
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "John", last_name: "Jerona", hourly_rate: 10)
+@store1.employees.create(first_name: "Jacob", last_name: "Verroa", hourly_rate: 14)
+@store1.employees.create(first_name: "Jingle", last_name: "Geona", hourly_rate: 16)
+@store2.employees.create(first_name: "Heimer", last_name: "Gienna", hourly_rate: 45)
+@store2.employees.create(first_name: "Schnidth", last_name: "Gianna", hourly_rate: 77)
+@store2.employees.create(first_name: "His", last_name: "Funny", hourly_rate: 23)
+@store2.employees.create(first_name: "Name", last_name: "Mikaelo", hourly_rate: 55)
+@store2.employees.create(first_name: "Is", last_name: "Mateo", hourly_rate: 8)
--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,23 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+class Store < ActiveRecord::Base
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { greater_than_or_equal_to: 0}
+end
+
+class Employee < ActiveRecord::Base
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :hourly_rate, :inclusion => 40..200
+  validates :store_id, presence: true
+end
+
+pp "Give me a store name"
+user_response = gets.chomp
+@store3 = Store.create(name: user_response)
+if @store3.errors.any?
+  @store3.errors.each do |attribute, message|
+    p message
+  end
+end
